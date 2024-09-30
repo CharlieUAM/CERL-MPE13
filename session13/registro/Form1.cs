93166e2293315e67132a1d2273a97cef5d449d80
Ivancast67 <icastillol@uamv.edu.ni>
2024-09-30 10:54:16 -0600

    Sesion 13 update

---
+++ session13/registro/Form1.cs
@@ -0,0 +1,37 @@
+﻿using registro.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace registro
+{
+    public partial class Form1 : Form
+    {
+        List<Persona> persons;
+        public Form1()
+        {
+            InitializeComponent();
+            persons = new List<Persona>();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            Persona person = new Persona();
+            person.name = tbprodct.Text;
+            person.price = int.Parse(tbprice.Text);
+            persons.Add(person);
+        }
+
+        private void ShowProducts()
+        {
+            DGVnumbers.DataSource = null;
+            DGVnumbers.DataSource = persons;
+        }
+    }
+}
---
+++ session13/registro/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace registro.Models
+{
+    internal class Persona
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+
+        
+    }
+}
